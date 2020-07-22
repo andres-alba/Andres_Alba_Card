0dbb048c61ea5adbe98d19a679cf84de5ac89255
Andres <andresalba34@gmail.com>
2020-07-22 15:38:30 -0400

    card for Andres Alba

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,103 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(title: Text("Andres' Portfolio")),
         backgroundColor: Colors.teal,
-        body: Container(),
+        bottomNavigationBar: BottomNavigationBar(
+          //onTap: onTabTapped,
+          items: [
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.attach_file),
+              title: Text('Attach'),
+            ),
+          ],
+        ),
+        body: SafeArea(
+          //SafeArea - display widget where it does not disturb clock, wifi icon, etc..
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                //backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/meeeee.jpeg'),
+              ),
+              Text(
+                'Andres Alba',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontFamily: 'SourceSans'),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade900,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_android,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+1 226 240 0813',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSans',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'andresalba34@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSans',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
